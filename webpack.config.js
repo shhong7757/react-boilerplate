41e5e35e8795aa4495ea03ae64f6df4f4ea352f2
sang hyeok hong <sanghyeokhong@sangui-MacBookAir.local>
2021-07-16 23:24:15 +0900

    feat: apply esbuild-loader with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 const path = require("path");
 const webpack = require("webpack");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
 
 module.exports = {
-  entry: ["react-hot-loader/patch", "./src/index.js"],
+  entry: ["react-hot-loader/patch", "./src/index.tsx"],
   mode: "development",
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] },
+        test: /\.js$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "jsx", // Remove this if you're not using JSX
+          target: "es2015", // Syntax to compile to (see options below for possible values)
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "tsx", // Or 'ts' if you don't need tsx
+          target: "es2015",
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "esbuild-loader",
+            options: {
+              loader: "css",
+              minify: true,
+            },
+          },
+        ],
       },
     ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
@@ -31,4 +52,12 @@ module.exports = {
     hotOnly: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
+  optimization: {
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: "es2015", // Syntax to compile to (see options below for possible values)
+        css: true, // Apply minification to CSS assets
+      }),
+    ],
+  },
 };
